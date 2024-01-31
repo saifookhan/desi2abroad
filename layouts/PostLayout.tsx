e17658365188e445c54af6a5c5c7a1432fe43d7a
saifookhan <isaifookhan@gmail.com>
2024-01-31 15:52:22 +0100

    typo fix

--- layouts/PostLayout.tsx
+++ layouts/PostLayout.tsx
@@ -95,7 +95,7 @@ export default function PostLayout({ content, authorDetails, next, prev, childre
             <div className="divide-y divide-gray-200 dark:divide-gray-700 xl:col-span-3 xl:row-span-2 xl:pb-0">
               <div className="prose max-w-none pb-8 pt-10 dark:prose-invert">{children}</div>
               <div className="pb-6 pt-6 text-sm text-gray-700 dark:text-gray-300">
-                <Link href={groupsUrl(path)} rel="nofollow">
+                <Link href={groupsUrl()} rel="nofollow">
                   Join our Whatsapp community
                 </Link>
                 {` • `}
