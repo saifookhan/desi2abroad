60e6894249f46383c0a69504ce7c8d66860bae68
saifookhan <isaifookhan@gmail.com>
2024-03-11 21:41:09 +0100

    add packing list blog

--- app/italy/page.tsx
+++ app/italy/page.tsx
@@ -1,6 +1,7 @@
 import SectionContainer from 'components/SectionContainer'
 import PageHeader from '@/components/general/page/PageHeader'
 import VideoPlayerCard from '@/components/general/Video/VideoPlayerCard'
+import dynamic from 'next/dynamic'
 
 //TODO:: fill meta
 // export const metadata = genPageMetadata({ title: 'Blog' })
@@ -17,6 +18,10 @@ const videosList = [
 ]
 
 export default function BlogPage() {
+  const Flow = dynamic(() => import('components/germany/Roadmap'), {
+    ssr: false,
+  })
+
   return (
     <>
       <PageHeader
@@ -25,6 +30,13 @@ export default function BlogPage() {
         headerColor="white"
       />
       <br />
+      <SectionContainer backgroundStyle={'lightGrey'}>
+        {/* //TODO:: Zoha */}
+        <Flow />
+        <div className="mb-8 text-black">
+          Credits: Hamza, Iraj A., Mariah M. Saifoo Khan, Sidra N., Yumna I., Zubair K.
+        </div>
+      </SectionContainer>
       <SectionContainer backgroundStyle="lightGrey">
         <div className="pt-8 pb-8">
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 lg:gap-4">
