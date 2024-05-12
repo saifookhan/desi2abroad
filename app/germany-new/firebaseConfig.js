79a5a533edb229994356c29cf7849e65697c0e15
saifookhan <isaifookhan@gmail.com>
2024-05-12 14:02:30 +0200

    linting

--- app/germany-new/firebaseConfig.js
+++ app/germany-new/firebaseConfig.js
@@ -1,20 +1,18 @@
-import { initializeApp } from "firebase/app";
-import { getDatabase, ref, get } from 'firebase/database';
+import { initializeApp } from 'firebase/app'
+import { getDatabase, ref, get } from 'firebase/database'
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCVwCEWiqqWeZ-MOVy4w72mD6PDxzEjvJ8",
-  authDomain: "desi2abroad-com.firebaseapp.com",
-  databaseURL: "https://desi2abroad-com-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "desi2abroad-com",
-  storageBucket: "desi2abroad-com.appspot.com",
-  messagingSenderId: "120248603952",
-  appId: "1:120248603952:web:1a7ede86f3c13748d7e4f5",
-  measurementId: "G-8DDJZ8QDD1"
-};
+  apiKey: 'AIzaSyCVwCEWiqqWeZ-MOVy4w72mD6PDxzEjvJ8',
+  authDomain: 'desi2abroad-com.firebaseapp.com',
+  databaseURL: 'https://desi2abroad-com-default-rtdb.europe-west1.firebasedatabase.app',
+  projectId: 'desi2abroad-com',
+  storageBucket: 'desi2abroad-com.appspot.com',
+  messagingSenderId: '120248603952',
+  appId: '1:120248603952:web:1a7ede86f3c13748d7e4f5',
+  measurementId: 'G-8DDJZ8QDD1',
+}
 
+const app = initializeApp(firebaseConfig)
+const db = getDatabase(app)
 
-  const app = initializeApp(firebaseConfig);
-  const db = getDatabase(app);
-
-  
-  export { db };
+export { db }
--- app/germany-new/page.tsx
+++ app/germany-new/page.tsx
@@ -1,73 +1,79 @@
 'use client'
-import React, { useEffect, useState } from 'react';
-import { get, ref } from 'firebase/database';
-import { Collapse } from 'antd';
-import { db } from './firebaseConfig';
+import React, { useEffect, useState } from 'react'
+import { get, ref } from 'firebase/database'
+import { Collapse } from 'antd'
+import { db } from './firebaseConfig'
 
-const { Panel } = Collapse;
+const { Panel } = Collapse
 
 const App: React.FC = () => {
-  const [data, setData] = useState<any[]>([]);
-  const [loading, setLoading] = useState(true); // Added loading state
+  const [data, setData] = useState<any[]>([])
+  const [loading, setLoading] = useState(true) // Added loading state
 
-  const parentStyler={
-    width:'60%',
-    margin:'50px auto',
-    border:'none',
-    color:'#fff'
+  const parentStyler = {
+    width: '60%',
+    margin: '50px auto',
+    border: 'none',
+    color: '#fff',
   }
-  const childStyle={
-    background:'#fff',
-    marginTop:'5px',
-    border:'none'
+  const childStyle = {
+    background: '#fff',
+    marginTop: '5px',
+    border: 'none',
   }
   useEffect(() => {
-    console.log('Fetching data...');
+    console.log('Fetching data...')
     const fetchData = async () => {
       try {
-        const dataRef = ref(db, '1hBI5WUxiwG06kncYRV7qhQcO52GqX5qYfup_zfBtN6Q/Sheet1');
-        const snapshot = await get(dataRef);
+        const dataRef = ref(db, '1hBI5WUxiwG06kncYRV7qhQcO52GqX5qYfup_zfBtN6Q/Sheet1')
+        const snapshot = await get(dataRef)
         console.log(snapshot.exists())
         if (snapshot.exists()) {
-          const dataArray: any[] = [];
+          const dataArray: any[] = []
           snapshot.forEach((childSnapshot) => {
-            const childData = childSnapshot.val();
-            dataArray.push(childData);
-          });
-          console.log('Data fetched:', dataArray);
-          setData(dataArray);
+            const childData = childSnapshot.val()
+            dataArray.push(childData)
+          })
+          console.log('Data fetched:', dataArray)
+          setData(dataArray)
         } else {
-          console.log('No data available');
+          console.log('No data available')
         }
-        setLoading(false); // Mark loading as false after data retrieval
+        setLoading(false) // Mark loading as false after data retrieval
       } catch (error) {
-        console.error('Error fetching data:', error);
-        setLoading(false); // Ensure loading is marked as false even if there's an error
+        console.error('Error fetching data:', error)
+        setLoading(false) // Ensure loading is marked as false even if there's an error
       }
-    };
-    
-    fetchData();
-  }, []);
+    }
+
+    fetchData()
+  }, [])
 
   if (loading) {
-    return <div>Loading...</div>;
+    return <div>Loading...</div>
   }
 
-  console.log('Data:', data); 
+  console.log('Data:', data)
 
   return (
     <div style={{ padding: '20px' }}>
       <Collapse accordion style={parentStyler}>
         {data.map((item: any) => (
           <Panel header={item.Question} key={item.ID} style={childStyle}>
-            <p><strong>Answer:</strong> {item.Answer}</p>
-            <p><strong>Topic:</strong> {item.Topic}</p>
-            <p><strong>Video Link:</strong> <a href={item.VideoLink}>{item.VideoLink}</a></p>
+            <p>
+              <strong>Answer:</strong> {item.Answer}
+            </p>
+            <p>
+              <strong>Topic:</strong> {item.Topic}
+            </p>
+            <p>
+              <strong>Video Link:</strong> <a href={item.VideoLink}>{item.VideoLink}</a>
+            </p>
           </Panel>
         ))}
       </Collapse>
     </div>
-  );
-};
+  )
+}
 
-export default App;
+export default App
