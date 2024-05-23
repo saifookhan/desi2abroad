2678a3ec5c7c5e1ac5a7851a7842286d3037b6c7
saifookhan <isaifookhan@gmail.com>
2024-05-23 17:28:46 +0200

    minor updates from Saif

--- app/germany-new/page.tsx
+++ app/germany-new/page.tsx
@@ -3,6 +3,8 @@ import React, { useEffect, useState } from 'react'
 import { Collapse } from 'antd'
 import axios from 'axios'
 import * as XLSX from 'xlsx'
+import PageHeader from '@/components/general/page/PageHeader'
+import SectionContainer from '@/components/SectionContainer'
 
 const { Panel } = Collapse
 
@@ -12,30 +14,31 @@ const App: React.FC = () => {
   const [loading, setLoading] = useState(true)
 
   // Link to your Google Sheets file
-  const GOOGLE_SHEET_URL = 'https://docs.google.com/spreadsheets/d/1hBI5WUxiwG06kncYRV7qhQcO52GqX5qYfup_zfBtN6Q/edit#gid=0'
+  const GOOGLE_SHEET_URL =
+    'https://docs.google.com/spreadsheets/d/1hBI5WUxiwG06kncYRV7qhQcO52GqX5qYfup_zfBtN6Q/edit#gid=0'
 
   const fetchDataFromGoogleSheet = async () => {
     try {
       const response = await axios.get(GOOGLE_SHEET_URL, {
-        responseType: 'arraybuffer', 
+        responseType: 'arraybuffer',
       })
       const dataBuffer = response.data
       const workbook = XLSX.read(dataBuffer, { type: 'buffer' })
       const sheetName = workbook.SheetNames[0]
       const sheet = workbook.Sheets[sheetName]
       const parsedData = XLSX.utils.sheet_to_json(sheet)
-      
+
       // Group topics by name
-      const groupedTopics: {[key: string]: any[]} = {}
-      parsedData.forEach(item => {
+      const groupedTopics: { [key: string]: any[] } = {}
+      parsedData.forEach((item) => {
         if (item.B) {
-          if (!groupedTopics[item.B]) { 
+          if (!groupedTopics[item.B]) {
             groupedTopics[item.B] = []
           }
           groupedTopics[item.B].push(item)
         }
       })
-      
+
       // Convert object of arrays into an array of arrays
       const groupedData = Object.values(groupedTopics)
       setFileData(groupedData)
@@ -47,9 +50,7 @@ const App: React.FC = () => {
   }
 
   const parentStyler = {
-    width: '60%',
     margin: '50px auto',
-    border: 'none',
     color: '#fff',
   }
   const childStyle = {
@@ -66,37 +67,51 @@ const App: React.FC = () => {
   }
 
   return (
-    <div style={{ padding: '20px' }}>
-      {/* Render fileData */}
-      <Collapse accordion style={parentStyler}>
-        {fileData.map((topicGroup: any[], index) =>{
-          console.log(topicGroup)
-         if(index!=0){
-          return(
-            (
-              <Panel header={topicGroup[0].B} key={index} style={childStyle}>
-                <Collapse accordion>
-                  {topicGroup.map((topic, subIndex) => (
-                    <Panel header={topic.C} key={subIndex} style={childStyle}>
-                      <p>
-                        <strong>Answer:</strong> {topic.D}
-                      </p>
-                      <p>
-                        <strong>Topic:</strong> {topic.B}
-                      </p>
-                      <p>
-                        <strong>Video Link:</strong> <iframe style={{width:'560px',height:'400px'}} src={topic.E}>{topic.E}</iframe>
-                      </p>
-                    </Panel>
-                  ))}
-                </Collapse>
-              </Panel>
-            )
-          )
-         }
-        })}
-      </Collapse>
-    </div>
+    <>
+      <PageHeader
+        headerTitle="Germany Study Steps"
+        headerDescription="Join our support communities"
+        headerColor="white"
+      />
+      <br />
+      <SectionContainer backgroundStyle="white">
+        <div className="">
+          <Collapse accordion style={parentStyler}>
+            {fileData.map((topicGroup: any[], index) => {
+              console.log(topicGroup)
+              if (index != 0) {
+                return (
+                  <Panel header={topicGroup[0].B} key={index} style={childStyle}>
+                    <Collapse accordion>
+                      {topicGroup.map((topic, subIndex) => (
+                        <Panel header={topic.C} key={subIndex} style={childStyle}>
+                          <p>
+                            <strong>Answer:</strong> {topic.D}
+                          </p>
+                          <div>
+                            <strong>Answer HTML:</strong>
+                            <div dangerouslySetInnerHTML={{ __html: topic.E }} />
+                          </div>
+                          <p>
+                            <strong>Topic:</strong> {topic.B}
+                          </p>
+                          <p>
+                            <strong>Video Link:</strong>{' '}
+                            <iframe style={{ width: '560px', height: '400px' }} src={topic.E}>
+                              {topic.E}
+                            </iframe>
+                          </p>
+                        </Panel>
+                      ))}
+                    </Collapse>
+                  </Panel>
+                )
+              }
+            })}
+          </Collapse>
+        </div>
+      </SectionContainer>
+    </>
   )
 }
 
